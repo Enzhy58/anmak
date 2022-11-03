dde0e59227f0e7d4bb0338a8f98840603bc92c7e
Enzhy58 <ang.borisova2015@yandex.ru>
2022-11-03 12:02:46 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
 $(function() {
+    $('.menu__btn').on('click', function(){
+        $('.menu__bottom-list').toggleClass('menu__bottom-list--active');
+    });
     $('.production__slider').slick({
         arrows: false,
         dots: true,
