bbdc06f7a3ab1bbaf09dc04a38fabcb3c9f97e67
Enzhy58 <ang.borisova2015@yandex.ru>
2022-11-08 00:18:56 +0300

    added form

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,33 @@ $(function() {
     $('.menu__btn').on('click', function(){
         $('.menu__bottom-list').toggleClass('menu__bottom-list--active');
     });
+    $('.menu__contect-email, .production__link, .footer__email').on('click', function(){
+        $('.form').addClass('form--active');
+        $('.wrapper').addClass('wrapper--active');
+    });
+    $('.form__close').on('click', function(){
+        $('.form').removeClass('form--active');
+        $('.wrapper').removeClass('wrapper--active');
+    });
     $('.production__slider').slick({
         arrows: false,
         dots: true,
         autoplay: true
     });
 });
+
+document.getElementById('home').onclick = function () {
+    window.location.href = 'index.html';
+};
+
+document.getElementById('contacts').onclick = function () {
+    window.location.href = 'contacts.html';
+};
+
+document.getElementById('about').onclick = function () {
+    window.location.href = 'about.html';
+};
+
+document.getElementById('gallery').onclick = function () {
+    window.location.href = 'gallery.html';
+};
